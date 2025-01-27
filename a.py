1219cf8b60d7dbb172ede37b675e40f38c3c593c
florijan127 <florijan.sandalj@gmail.com>
2025-01-27 03:19:19 +0100

    promjena znacajki kroz faze

--- a.py
+++ a.py
@@ -1,23 +1,13 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
-import os
+import matplotlib.pyplot as plt
 
-for i in range(1, 21):
-    if os.path.exists(f'EDA/eda_{i:02}.csv'):
-        eda = pd.read_csv(f'EDA/eda_{i:02}.csv')
-        len_eda = len(eda)
-    if os.path.exists(f'RESPIRATION/respiration_{i:02}.csv'):
-        resp = pd.read_csv(f'RESPIRATION/respiration_{i:02}.csv')
-        len_resp = len(resp)
-    if os.path.exists(f'TIME/time_{i:02}.csv'):
-        time = pd.read_csv(f'TIME/time_{i:02}.csv')
-        len_time = len(time)
-    
-    if len_eda == len_resp == len_time:
-        print(f'equal for {i}')
-    else:
-        print(f'not equal for {i}')
-        print(f'len({i} = {len(eda)}')
-        print(f'len({i} = {len(resp)}')
-        print(f'len({i} = {len(time)}')
+plt.figure(figsize=(20, 10))
+plt.scatter(['a', 'b', 'c'], [1, 2, 1], color='red')
+plt.plot(['a', 'b', 'c'], [1, 2, 1], marker='o')
+plt.scatter(['a', 'b', 'c'], [1, 3, 1], color='red')
+plt.plot(['a', 'b', 'c'], [1, 3, 1], marker='o')
+plt.scatter(['a', 'b', 'c'], [1, 4, 1], color='red')
+plt.plot(['a', 'b', 'c'], [1, 4, 1], marker='o')
 
+plt.show()
---
+++ promjena_znacajki_vizualizacija.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+znacajke = pd.read_csv("znacajke.csv")
+print(len(znacajke))
+
+plt.figure(figsize=(20, 10))
+
+for i, feature in enumerate(['EDAmean', 'EDAstd', 'EDAslope', 'EDArange', 'RESPrate', 'RESPamplitude', 'RESPstd', 'RESPrange']):
+    plt.subplot(1, 8, i + 1)
+    plt.title(feature)
+    for id in znacajke['id'].unique():
+        y = []
+        x = ['pre', 'startle', 'post']
+        for phase in x:
+            print(znacajke[(znacajke['id'] == id) & (znacajke['phase'] == phase) & (znacajke['startle_index'] == 1)][feature])
+            y.append(znacajke[(znacajke['id'] == id) & (znacajke['phase'] == phase) & (znacajke['startle_index'] == 1)][feature])
+        plt.plot(x, y, color='red', marker='o')
+        plt.scatter(x, y, color='red') 
+    plt.legend()
+        
+plt.show()
